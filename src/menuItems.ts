6ea965915af4f53b4e6f82799a2563ff61a62d5c
sgwicks <samgwicks@gmail.com>
2022-11-25 17:21:47 +0000

    Add hardcoded meals

--- src/menuItems.ts
+++ src/menuItems.ts
@@ -1,61 +1,120 @@
 /**
 export const menuItem: MenuItem = {
   main: '',
-  carbs: [],
   options: [],
+  carbs: [],
   extras: []
 }
  */
 
 export const tikkaMasala: MenuItem = {
   main: "Tikka Masala",
-  carbs: ['rice', 'naan', 'bhaji'],
   options: ['chickpea', 'tofu'],
+  carbs: ['rice', 'naan', 'bhaji'],
   extras: ['bhaji']
 }
 
 export const cheeseSauce: MenuItem = {
   main: 'Cheese Sauce',
-  carbs: ['pasta'],
   options: [],
+  carbs: ['pasta'],
   extras: ['breadcrumbs', 'tomato', 'bacon']
 }
 
 export const blackBeanChilli: MenuItem = {
   main: 'Black Bean Chilli',
-  carbs: ['rice', 'bread', 'naan', 'chips', 'nachos'],
   options: [],
+  carbs: ['rice', 'bread', 'naan', 'chips', 'nachos'],
   extras: ['bell pepper', 'tomato']
 }
 
-export const menuItem: MenuItem = {
-  main: '',
-  carbs: [],
+export const spinachCurry: MenuItem = {
+  main: 'Spinach Curry',
   options: [],
-  extras: []
+  carbs: ['rice',	'naan',	'bhaji'],
+  extras: ['bhaji']
 }
-export const menuItem2: MenuItem = {
-  main: '',
-  carbs: [],
-  options: [],
-  extras: []
+
+export const bolognese: MenuItem = {
+  main: 'Bolognese',
+  options: ['lentil', 'mince'],
+  carbs: ['spaghetti', 'pasta'],
+  extras: ['tomato', 'mushroom', 'carrot']
 }
-export const menuItem3: MenuItem = {
-  main: '',
-  carbs: [],
+
+export const goulash: MenuItem = {
+  main: 'Goulash',
   options: [],
-  extras: []
+  carbs: ['bread', 'naan', 'rice', 'bulgur wheat'],
+  extras: ['soya chunks']
 }
-export const menuItem4: MenuItem = {
-  main: '',
-  carbs: [],
-  options: [],
+
+export const dhal: MenuItem = {
+  main: 'Dhal',
+  options: ['red lentil', 'yellow lentil', 'green lentil'],
+  carbs: ['naan', 'pitta', 'rice'],
   extras: []
 }
 
-export const menuItem5: MenuItem = {
-  main: '',
-  carbs: [],
+export const spicyChickpeas: MenuItem = {
+  main: 'Spicy Chickpeas',
   options: [],
+  carbs: ['pitta', 'rice', 'cous cous', 'bulgur wheat'],
   extras: []
+}
+
+export const sataySauce: MenuItem = {
+  main: 'Satay Sauce',
+  options: ['tofu', 'chickpea'],
+  carbs: ['pitta', 'rice'],
+  extras: []
+}
+
+export const pizza: MenuItem = {
+  main: 'Pizza',
+  options: [],
+  carbs: [],
+  extras: ['chips', 'sausage']
+}
+
+export const cousCousBowl: MenuItem = {
+  main: 'Cous Cous Bowl',
+  options: ['cous cous', 'bulgur wheat'],
+  carbs: ['pitta'],
+  extras: ['leaves', 'tofu', 'bhaji']
+}
+
+export const lasagne: MenuItem = {
+  main: 'Lasagne',
+  options: ['lentil', 'nacho', 'mince'],
+  carbs: [],
+  extras: ['chips']
+}
+
+export const tabbouleh: MenuItem = {
+  main: '',
+  options: [],
+  carbs: ['pitta'],
+  extras: []
+}
+
+export const tofu: MenuItem = {
+  main: 'Tofu',
+  options: ['marinated', 'dry-fried'],
+  carbs: ['pitta', 'rice', 'bread'],
+  extras: []
+}
+
+export const sosmix: MenuItem = {
+  main: 'Sosmix',
+  options: [],
+  carbs: ['bread', 'chips'],
+  extras: []
+}
+
+export const burgers: MenuItem = {
+  main: 'Homemade Burgers',
+  options: ['seitan', 'bean'],
+  carbs: ['bread', 'chips'],
+  extras: ['chips', 'beans']
 }
